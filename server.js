19e62be3feeae28ffa403420a66881152cdc3df8
MarinaShemesh <marina.shemesh1@gmail.com>
2017-03-09 14:32:11 +0200

    number 1

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser'); 
 var app = express();
+var mongoose = require('mongoose');
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); 
 
 app.listen(8000, function() {
   console.log("Fullstack project. Listening on 8000.")
