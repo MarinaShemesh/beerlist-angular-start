dd8027ef75886a3b0ca0f1135c7772ec4309739c
MarinaShemesh <marina.shemesh1@gmail.com>
2017-03-09 13:59:33 +0200

    test

---
+++ public/js/controllers/mainController.js
@@ -0,0 +1,22 @@
+app.controller ('mainController', function ($scope, beers){
+
+  $scope.newBeers = beers.newBeers;
+  $scope.addBeer = beers.addBeer;
+//assign service data and functions to scope object
+
+
+// $scope.newBeer= [];
+//    $scope.addBeer= function(){
+    
+//     alert("button works");
+//     var ale = {
+
+//      img:$scope.img,
+//      name:$scope.name,
+//      style:$scope.style,
+//      abv:$scope.abv
+//     };
+
+//     beers.addBeer(newBeer);
+//  }
+ });
---
+++ public/js/services/beers.js
@@ -0,0 +1,44 @@
+app.service('beers', function(){
+  var newBeers = [
+        {
+          img:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQHnrjPN57ATG1OdT8EK-CCABjrxizna7Hb9MusZcKiR6H1zQMsAw", 
+          name:"Kentucky Brunch Brand", 
+          style:"Stout", 
+          abv: 4.35,
+          rating: 4
+          },
+        
+        {
+          img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM1FkMYyv-S6adwzarKtitQBc7P_KT6sNzjEEXrGDrxcZNdi7q", 
+          name:"Very Hazy", 
+          style: "IPA", 
+          abv: 4.77,
+          rating: 2
+          },
+
+        {
+          img: "https://c1.staticflickr.com/9/8260/8605518012_86816323e0_n.jpg", 
+          name:"Pale Writer", 
+          style: "IPA", 
+          abv: 4.67,
+          rating: 5
+          }
+        
+  ];
+
+  var test = 'this is from the service';
+
+  var addBeer = function(stout){
+    console.log(stout)
+    stout.rating = 0;
+    newBeers.push(stout)
+    console.log(newBeers);
+     }
+
+return {
+    newBeers: newBeers,
+    test: test, //remember the second test is the var above
+    addBeer:addBeer
+};
+});
+//define the functions
